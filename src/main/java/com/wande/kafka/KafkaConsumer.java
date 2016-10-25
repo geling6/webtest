24f794cd35c988f208365cd843e71e923e597448
geling <ningxiner66@126.com>
2016-10-25 16:18:40 +0800

    ccc

--- src/main/java/com/wande/kafka/KafkaConsumer.java
+++ src/main/java/com/wande/kafka/KafkaConsumer.java
@@ -14,16 +14,16 @@ import kafka.javaapi.consumer.ConsumerConnector;
 public class KafkaConsumer {
 	public static void main(String[] args) {
 		Properties props = new Properties();
-		props.put("zookeeper.connect", "10.213.42.140:12181,10.213.42.141:12181,10.213.42.142:12181/ffan/kafka/ffan_service/allinone");
+		props.put("zookeeper.connect", "");
 		props.put("group.id", "group1");
 		
 		ConsumerConfig consumerConfig = new ConsumerConfig(props);
 		ConsumerConnector consumerConnector = Consumer.createJavaConsumerConnector(consumerConfig);
 		
 		Map<String,Integer> map = new HashMap<>();
-		map.put("cda-merchant", 1);
+		map.put("", 1);
 		Map<String,List<KafkaStream<byte[],byte[]>>> messageStreams = consumerConnector.createMessageStreams(map);
-		KafkaStream<byte[],byte[]> messageStream = messageStreams.get("cda-merchant").get(0);
+		KafkaStream<byte[],byte[]> messageStream = messageStreams.get("").get(0);
 		ConsumerIterator<byte[],byte[]> iterator = messageStream.iterator();
 		while(iterator.hasNext()){
             String message = new String(iterator.next().message());  
