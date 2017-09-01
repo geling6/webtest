51cb7b3e0f9a8a74b56b9d3babc737e574b73222
geling <ningxiner66@126.com>
2017-09-01 15:50:30 +0800

    redis集群测试

--- src/main/java/com/wande/redis/JedisSpringTest.java
+++ src/main/java/com/wande/redis/JedisSpringTest.java
@@ -1,7 +1,10 @@
 package com.wande.redis;
 
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +12,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.CollectionUtils;
 
 import com.wande.mybatis.bean.Store;
 import com.wande.mybatis.dao.StoreMapper;
@@ -38,10 +42,32 @@ public class JedisSpringTest {
 		
 		StoreMapper mapper = session.getMapper(StoreMapper.class);
 		int count = mapper.count();
-		List<Store> stores = mapper.queryStores(0, 100);
 		
-		System.out.println(count + "" + stores);
+		Class<?> storeClass = Class.forName("com.wande.mybatis.bean.Store");
+		Field[] fields = Store.class.getDeclaredFields();
 		
+		int offset = 0, limit = 100;
+		while(offset <= count) {
+			List<Store> stores = mapper.queryStores(offset, limit);
+			for(Store store : stores) {
+				String key = "STORE_" + store.getStoreId();
+				Map<String, String> old = jedisCluster.hgetAll(key);
+				if(!CollectionUtils.isEmpty(old)) {
+					for(Field field : fields) {
+						String fieldSetter = field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+						Method met = storeClass.getMethod("get" + fieldSetter);
+						Object value = met.invoke(store);
+						if(value != null) {
+							jedisCluster.hsetnx(key, field.getName(), value.toString());
+						}
+					}					
+				}
+			}
+			
+			System.out.println("offset:" + offset + ", limit" + limit);
+			offset += limit;
+		}
+				
 	}
 
 }
